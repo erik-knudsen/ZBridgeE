ff0f703d21e2068b68bcb0fe10d39bd5458e96b0
Erik Aagaard Knudsen <eak.erik@gmail.com>
2018-10-10 11:14:37 +0200

    Changed card distribution.

--- ZBridgeE/cplayengine.cpp
+++ ZBridgeE/cplayengine.cpp
@@ -84,9 +84,9 @@ int CPlayEngine::getNextPlay(Seat seat, Seat dummySeat, int ownCards[], int dumm
     if (playHistory.getNoTrick() >= 2)
         noHands += 20;
     if (playHistory.getNoTrick() >= 5)
-        noHands += 20;
+        noHands += 10;
     if (playHistory.getNoTrick() >= 8)
-        noHands += 20;
+        noHands += 10;
     if (playHistory.getNoTrick() >= 11)
         noHands = noHandsDD;
     if (noHands > MAX_NO_HANDS)
